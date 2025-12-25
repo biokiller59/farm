29fd4ae2e8ea9ba8fc2ac4105d070c82c2d4b64d
romankozeiko-cmd <romankozeiko@gmail.com>
2025-12-26 00:42:40 +0300

    Update farm.lua

--- farm.lua
+++ farm.lua
@@ -1,15 +1,15 @@
 while true do
-wait(20)
-game:GetService("Workspace").riperinside.HumanoidRootPart.CFrame = game:GetService("Workspace").Bosses.Waiting.Titan.qw.CFrame
-wait(11)
+wait(15)
+game:GetService("Workspace").biokiller60.HumanoidRootPart.CFrame = game:GetService("Workspace").Bosses.Waiting.Titan.qw.CFrame
+wait(8)
 if game:GetService("Workspace").RespawnMobs.Titan.Titan.Titan  then
-	game:GetService("Workspace").riperinside.HumanoidRootPart.CFrame = game:GetService("Workspace").RespawnMobs.Titan.Titan.CFrame
+	game:GetService("Workspace").biokiller60.HumanoidRootPart.CFrame = game:GetService("Workspace").RespawnMobs.Titan.Titan.CFrame
 end
-wait (20)
-game:GetService("Workspace").riperinside.HumanoidRootPart.CFrame = game:GetService("Workspace").Bosses.Waiting.Muscle.qw.CFrame
-wait(11)
+wait (15)
+game:GetService("Workspace").biokiller60.HumanoidRootPart.CFrame = game:GetService("Workspace").Bosses.Waiting.Muscle.qw.CFrame
+wait(8)
 if game:GetService("Workspace").RespawnMobs.Muscle.Muscle then
-	game:GetService("Workspace").riperinside.HumanoidRootPart.CFrame = game:GetService("Workspace").RespawnMobs.Muscle.Muscle.CFrame
+	game:GetService("Workspace").biokiller60.HumanoidRootPart.CFrame = game:GetService("Workspace").RespawnMobs.Muscle.Muscle.CFrame
 end
 task.wait(1)
 end
